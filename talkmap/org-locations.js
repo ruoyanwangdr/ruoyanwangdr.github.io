93f770f07addd33590e0554d8ccf047faaa8f262
rywjhzd <rywang@strw.leidenuniv.nl>
2020-03-24 15:13:32 +0100

    Update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,16 @@
 var addressPoints = [
   [
-    "Santa Cruz, CA, USA | 7th Annual Physical and Biological Sciences Summer Research Symposium | <a href='https://rywjhzd.github.io/talks/2016-08-16-ucsc'>Poster Presentation</a>",
+    "Santa Cruz, CA, USA | 7th Annual Physical and Biological Sciences Summer Research Symposium | <a href='https://rywjhzd.github.io/talks/2016-08-16-ucsc'>Models of Brown Dwarfs Atmospheres</a>",
     37.000520,
     -122.063146
   ],
   [
-    "Santa Cruz, CA, USA | 8th Annual Physical and Biological Sciences Summer Research Symposium | <a href='https://rywjhzd.github.io/talks/2017-08-18-ucsc'>Poster Presentation</a>",
+    "Santa Cruz, CA, USA | 8th Annual Physical and Biological Sciences Summer Research Symposium | <a href='https://rywjhzd.github.io/talks/2017-08-18-ucsc'>Modeling Brown Dwarfs Atmospheres</a>",
     37.000520,
     -122.063146
   ],
   [
-    "Washington, DC, USA | 231st Meeting of American Astronomical Society | <a href='https://rywjhzd.github.io/talks/2018-01-12-aas-231'>Poster Presentation</a>",
+    "Washington, DC, USA | 231st Meeting of American Astronomical Society | <a href='https://rywjhzd.github.io/talks/2018-01-12-aas-231'>Atmospheres of Brown Dwarfs</a>",
     38.781580,
     -77.016773
   ],
